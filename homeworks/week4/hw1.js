fd1dcf78a749e1face931b0e9e7c2c5464b2c917
Andrea Hsu <andrea.hsu1030@gmail.com>
2021-05-07 11:58:26 +0800

    h1

---
+++ homeworks/week4/hw1.js
@@ -0,0 +1,19 @@
+const request = require('request')
+
+request('https://lidemy-book-store.herokuapp.com/books?_limit=10', (error, response, body) => {
+  if (error) {
+    console.log('error', error)
+    return
+  }
+  let books
+  try {
+    books = JSON.parse(body)
+  } catch (error) {
+    console.log(error)
+    return
+  }
+
+  for (let i = 0; i < books.length; i++) {
+    console.log(`${books[i].id}  ${books[i].name}`)
+  }
+})
