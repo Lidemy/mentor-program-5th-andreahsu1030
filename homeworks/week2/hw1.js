a5c90053a07d18bb3b06b3e4b7c14d1824233269
Andrea Hsu <andrea.hsu1030@gmail.com>
2021-08-09 01:53:06 +0800

    test

--- homeworks/week2/hw1.js
+++
@@ -1,7 +0,0 @@
-/* eslint-disable */
-function printStars(n) {
-  for (var i=0; i<n && i<30; i++){
-  console.log('*')
-  }
-}
-printStars(5)
--- homeworks/week2/hw3.js
+++
@@ -1,9 +0,0 @@
-/* eslint-disable */
-function reverse(str) {
-  let ans = ""
-  for(let i = str.length -1 ; i >= 0; i-- ){
-    ans += str[i]
-    }
-return ans
-}
-console.log(reverse('hello'))
